2ab8fc8d023510fc89f6a25124fe67f2b094f2ca
santi <santi@JULIAN>
2022-08-08 21:15:18 -0600

    Agregar archivos de proyecto.

---
+++ reporC++/main.cpp
@@ -0,0 +1,30 @@
+//******************************************************************************//
+// Plantilla Proyecto Vacio C++ v1.0 (08/08/2022)								//
+//******************************************************************************//
+//																				//
+//General	> Dir Salida		> $(SolutionDir)\Bin\							//
+//			> Dir Intermedio	> $(SolutionDir)\Tmp							//
+//			> Nomb Destino		> $(ProjectName)				(Release/64)	//
+//			> Nomb Destino		> $(ProjectName)_d				(Debug/64)		//
+//			> Nomb Destino		> $(ProjectName)_32				(Release/32)	//
+//			> Nomb Destino		> $(ProjectName)_32d			(Debug/32)		//
+//																				//
+//Avanzado	> Juego de caract	> Multibyte										//
+//C/C++		> General			> Comp multiproc		> Si					//
+//			> Optimizacion		> Tamaño o velocidad    > Codigo Rapido			//
+//			> Gen de codigo		> Bid en tiempo de ej   > MT (Release)			//
+//			>					> Bid en tiempo de ej   > MTd (Debug)			//
+//			>					> Bid en tiempo de ej   > MT (Release)			//
+//			>					> Modelo de punt f1		> Rapido				//
+//			> Idioma			> Estandar del Leng		> Ultimo Borrador		//
+//																				//
+//******************************************************************************//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hola mundo!!" << endl;
+	cin.get();
+	return 0;
+}
